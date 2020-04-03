1417820849a562c34ff4432b00c100331bbfa62f
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-04-03 22:48:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@
 def badge_maker(name) 
   return  "Hello, my name is #{name}." 
 end
- 
+
 conference_speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace","Linus","Matz"]
 
 def batch_badge_creator(attendees)
@@ -34,3 +34,7 @@ end
   
 
 
+def batch_badge_creator(name)
+ conference_speakers = []
+ conference_speakers.each do |name|
+    return "#{badge_maker(name)}"
